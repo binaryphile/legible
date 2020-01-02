553015670fcb679deccef2bc9fc18020a0d53a7e
Ted Lilley <ted.lilley@digi.com>
2020-01-02 16:21:30 -0500

    hash.empty?

--- hash.bash
+++ hash.bash
@@ -1,8 +1,15 @@
 # hash.bash - hash (associative array) functions
 
+# empty? returns true if the hash has no items and false otherwise
+hash.empty? () {
+  local -n Name=$1
+
+  (( ${#Name[*]} ))
+}
+
 # keys lists the keys of the given hashname
 hash.keys () {
-  local -n Hashname=$1
+  local -n Name=$1
 
-  echo "${!Hashname[*]}"
+  echo "${!Name[*]}"
 }
